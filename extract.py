6b1e81e233d7b858e29f94e5ceb3b3f9e90e17f2
kevinpanaro <panaro.kevin@gmail.com>
2015-03-02 19:02:59 -0500

    Add new search functions

--- extract.py
+++ extract.py
@@ -4,13 +4,36 @@ import ujson
 import pprint
 
 
-data = ujson.load(open('drexel_best.json'))
+f = ujson.load(open('scraped_data/drexel_best.json'))
 
 
-pp = pprint.PrettyPrinter(indent=4)
-pprint.pprint(data, depth=5)
+# pprint.pprint(f, depth=2)
+term = "Spring Quarter 14-15"
+course = "MATH" # technically also Semester?
+number = "101"
 
 
+def search_class(json, search):
+    '''returns class dictionary (add technically term)'''
+    temp_jawn = []
+    for size in range(len(json)):
+        if json.keys()[size] == search:
+            return json[search]
+    last_key = json.keys()[size]
+    return search_class(json[last_key], search)
+        
+
+def all_number_return(json, search):
+    '''returns all classes with number XXX'''
+    class_number_dict = {}
+    for term in json:
+        for course in json[term].keys():
+            for num in json[term][course].keys():
+                if num == search:
+                    class_number_dict[course] = {num: json[term][course][num]}
+
+    return class_number_dict
+        
 
 
 
