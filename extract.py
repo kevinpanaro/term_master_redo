599031b62a3916225bc4cb8e670f20ed16d4f4d4
kevinpanaro <panaro.kevin@gmail.com>
2015-03-04 13:48:14 -0500

    add more search options

--- extract.py
+++ extract.py
@@ -9,8 +9,9 @@ f = ujson.load(open('scraped_data/drexel_best.json'))
 
 # pprint.pprint(f, depth=2)
 term = "Spring Quarter 14-15"
-course = "MATH" # technically also Semester?
+course = "MATH".upper()  # technically also Semester?
 number = "101"
+joined = "ECON 201".upper()
 
 
 def search_class(json, search):
@@ -21,19 +22,32 @@ def search_class(json, search):
             return json[search]
     last_key = json.keys()[size]
     return search_class(json[last_key], search)
-        
+
 
 def all_number_return(json, search):
     '''returns all classes with number XXX'''
-    class_number_dict = {}
+    return_dict = {}
     for term in json:
         for course in json[term].keys():
             for num in json[term][course].keys():
                 if num == search:
-                    class_number_dict[course] = {num: json[term][course][num]}
+                    return_dict[course] = {num: json[term][course][num]}
 
-    return class_number_dict
-        
-
+    return return_dict
 
 
+def joined_search(json, search):
+    '''returns search from CLASS ###'''
+    split_text = re.split(" ", search)
+    if len(split_text) != 2:
+        return "Please enter a valid course description (ie. MATH 101)"
+    for item in split_text:
+        if len(item) == 3 and item.isdigit() is True:
+            number = item
+        else:
+            course = item
+    number_dict = all_number_return(json, number)
+    return search_class(number_dict, course)
+
+
+pprint.pprint(joined_search(f, joined))
